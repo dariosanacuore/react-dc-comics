6858f87f36c4f38126098c657e603cde0c347926
dariosanacuore <dariosanacuore2@gmail.com>
2025-11-26 15:51:12 +0100

    feat:modificata navbar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,25 +1,24 @@
 import style from "./Header.module.css";
 import logo from "../assets/img/dc-logo.png";
+
+
 function AppHeader() {
+    const links = ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"];
+
     return (
         <header>
             <div className="logo">
                 <img src={logo} alt="logo" />
+
             </div>
-            <div>
-                <ul className="menu">
-                    <li>CHARACTERS</li>
-                    <li>COMICS</li>
-                    <li>MOVIES</li>
-                    <li>TV</li>
-                    <li>GAMES</li>
-                    <li>COLLECTIBLES</li>
-                    <li>VIDEOS</li>
-                    <li>FANS</li>
-                    <li>NEWS</li>
-                    <li>SHOP</li>
-                </ul>
-            </div>
+            <ul>
+                {links.map((curLink, index) => (
+                    <li key={index}>
+                        {curLink}
+                    </li>
+                ))}
+
+            </ul>
         </header>
     );
 }
